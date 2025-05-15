e0d67cb3e8c76d352c80831a1b56866d3c1d1822
Gabriel SC <admgabriel512@gmail.com>
2025-05-15 17:51:44 -0300

    banco de dados
    Adicionei o Comando SQL para criar o banco de dados, e fiz o modelo do usuario

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/model/Usuario.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/model/Usuario.java
@@ -1,10 +1,63 @@
 
 package com.mycompany.mavenproject1.model;
-
 /**
  *
  * @author Gabriel s.c
  */
 public class Usuario {
     
+    private int id;
+    private String nome;
+    private int idade;
+    private boolean administrador;
+    private String login;
+    private String senha;
+    
+    public Usuario(int id, String nome, int idade, boolean administrador, String login, String senha){
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.administrador = administrador;
+        this.login = login;
+        this.senha = senha;
+    }
+    //sets
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public void setAdministrador(boolean administrador){
+        this.administrador = administrador;        
+    }
+    public void setLogin(String login){
+        this.login =  login;
+    }
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+    
+    ///gets
+    public int getId(){
+        return this.id;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public boolean getAdministrador(){
+        return this.administrador;
+    }
+    public String getLogin(){
+        return this.login;
+    }
+    public String getSenha(){
+        return this.senha;
+    }
 }
